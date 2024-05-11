c271f74ecc2be4f4b548a7f6448f740d5552ccf9
161Nikita <157229177+161Nikita@users.noreply.github.com>
2024-05-11 11:51:04 +0300

    login.cy.js

--- login.cy.js
+++ login.cy.js
@@ -2,8 +2,8 @@
 describe('Автотесты на форму логина', function () {
    it('Проверка авторизации с верным логином и паролем', function () {
         cy.visit('https://login.qa.studio/'); // зайти на сайт
-        cy.get('#mail').type ('german@dolnikov.ru'); // ввести верный логин
-        cy.get('#pass').type ('iLoveqastudio1'); // ввести верный пароль
+        cy.get('#mail').type ('german@dlnik.ru'); // ввести верный логин
+        cy.get('#pass').type ('Loveqastudi'); // ввести верный пароль
         cy.get('#loginButton').click(); // нажать кнопку войти
         cy.get('#messageHeader').contains('Авторизация прошла успешно'); // текст совпадает
         cy.get('#exitMessageButton > .exitIcon').should ('be.visible'); // кнопка крестик видна
@@ -13,7 +13,7 @@ describe('Автотесты на форму логина', function () {
     it('Проверка логики восстановления пароля', function () {
         cy.visit('https://login.qa.studio/'); // зайти на сайт
         cy.get('#forgotEmailButton').click(); // нажать кнопку забыли пароль
-        cy.get('#mailForgot').type ('ger@dol.ru'); // ввести любую почту
+        cy.get('#mailForgot').type ('german@dlnik.ru'); // ввести любую почту
         cy.get('#forgotForm > .header').contains('Восстановите пароль'); // текст совпадает
         cy.get('#exitRestoreButton > .exitIcon').should ('be.visible'); // кнопка крестик видна
 
@@ -21,8 +21,8 @@ describe('Автотесты на форму логина', function () {
 
    	it('Проверка авторизации с верным логином и неверным паролем', function () {
         cy.visit('https://login.qa.studio/'); // зайти на сайт
-        cy.get('#mail').type ('german@dolnikov.ru'); // ввести верный логин
-        cy.get('#pass').type ('iLoveqastudio5'); // ввести неверный пароль
+        cy.get('#mail').type ('german@dlnik.ru'); // ввести верный логин
+        cy.get('#pass').type ('Loveqastudi'); // ввести неверный пароль
         cy.get('#loginButton').click(); // нажать кнопку войти
         cy.get('#messageHeader').contains('Такого логина или пароля нет'); // текст совпадает
         cy.get('#exitMessageButton > .exitIcon').should ('be.visible'); // кнопка крестик видна
@@ -31,8 +31,8 @@ describe('Автотесты на форму логина', function () {
 
     it('Проверка авторизации с неверным логином и верным паролем', function () {
         cy.visit('https://login.qa.studio/'); // зайти на сайт
-        cy.get('#mail').type ('german@dnikov.ru'); // ввести неверный логин
-        cy.get('#pass').type ('iLoveqastudio1'); // ввести верный пароль
+        cy.get('#mail').type ('german@dlnik.ru'); // ввести неверный логин
+        cy.get('#pass').type ('Loveqastudi'); // ввести верный пароль
         cy.get('#loginButton').click(); // нажать кнопку войти
         cy.get('#messageHeader').contains('Такого логина или пароля нет'); // текст совпадает
         cy.get('#exitMessageButton > .exitIcon').should ('be.visible'); // кнопка крестик видна
@@ -41,8 +41,8 @@ describe('Автотесты на форму логина', function () {
 
     it('Проверка валидации без @ в логине', function () {
         cy.visit('https://login.qa.studio/'); // зайти на сайт
-        cy.get('#mail').type ('germandnikov.ru'); // ввести неверный логин
-        cy.get('#pass').type ('iLoveqastudio1'); // ввести верный пароль
+        cy.get('#mail').type ('german@dlnik.ru'); // ввести неверный логин
+        cy.get('#pass').type ('Loveqastudi'); // ввести верный пароль
         cy.get('#loginButton').click(); // нажать кнопку войти
         cy.get('#messageHeader').contains('Нужно исправить проблему валидации'); // текст совпадает
 
@@ -50,8 +50,8 @@ describe('Автотесты на форму логина', function () {
 
     it('Проверка приведения к строчным буквам в логине', function () {
         cy.visit('https://login.qa.studio/'); // зайти на сайт
-        cy.get('#mail').type ('GerMan@Dolnikov.ru'); // ввести логин с разным регистром
-        cy.get('#pass').type ('iLoveqastudio1'); // ввести верный пароль
+        cy.get('#mail').type ('german@dlnik.ru'); // ввести логин с разным регистром
+        cy.get('#pass').type ('Loveqastudi'); // ввести верный пароль
         cy.get('#loginButton').click(); // нажать кнопку войти
         cy.get('#messageHeader').contains('Авторизация прошла успешно'); // текст совпадает
         cy.get('#exitMessageButton > .exitIcon').should ('be.visible'); // кнопка крестик видна
